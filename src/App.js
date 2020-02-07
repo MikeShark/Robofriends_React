7073c27d3afcc7531686c7f676e5e34552ae7663
Mike Polozov <mikhail.polozov@mera.com>
2020-02-07 17:46:14 +0300

    Fetching data via http request works

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
-import './App.css';
+// import { robots } from "./robots";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: ""
     };
+  }
+
+  componentDidMount() {
+    // this.setState({ robots: robots })
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => {
+        this.setState({ robots: users });
+        console.log(users);
+      });
   }
 
   onSearchChange = event => {
@@ -25,7 +35,7 @@ class App extends Component {
     });
     return (
       <div className="tc">
-        <h1 className='f1'>RoboFriends</h1>
+        <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>
